1ee0325a7f0dd533a50096e128396d1402ac7f1b
Rishabh Sharma <2022uce0024@iitjammu.ac.in>
2025-07-31 17:29:10 +0530

    implemented logout

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,157 +1,163 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import getDataUri from "../utils/dataUri.js";
+import cloudinary from "../utils/cloudinary.js";
 
-export const register =async(req,res)=>{
-    try{
-        const{fullname,email,phoneNumber,password,role}=req.body;
-        if(!fullname || !email || !phoneNumber || !password || !role){
+export const register = async (req, res) => {
+    try {
+        const { fullname, email, phoneNumber, password, role } = req.body;
+        if (!fullname || !email || !phoneNumber || !password || !role) {
             return res.status(400).json({
-                message:"Something is missing",
-                success:false
+                message: "Something is missing",
+                success: false
             });
         };
-        const user =await User.findOne({email});
-        if(user){
+        const user = await User.findOne({ email });
+        if (user) {
             return res.status(400).json({
-                message:"User already exists with this email.",
-                success:false 
+                message: "User already exists with this email.",
+                success: false
             });
         }
-        const hashedPassword=await bcrypt.hash(password,10); 
+        const hashedPassword = await bcrypt.hash(password, 10);
         await User.create({
             fullname,
             email,
             phoneNumber,
-            password:hashedPassword,
+            password: hashedPassword,
             role,
         });
 
         return res.status(201).json({
-            message:"Account created successfully",
-            success:true
+            message: "Account created successfully",
+            success: true
         });
-       }catch(error){
-console.log(error);
-       }
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-export const login =async(req,res)=>{
-    try{
-const {email,password,role}=req.body;
- if(!email || !password || !role){
+export const login = async (req, res) => {
+    try {
+        const { email, password, role } = req.body;
+        if (!email || !password || !role) {
             return res.status(400).json({
-                message:"Something is missing",
-                success:false
+                message: "Something is missing",
+                success: false
             });
         };
-        let user =await User.findOne({email});
-        if(!user){
+        let user = await User.findOne({ email });
+        if (!user) {
             return res.status(400).json({
-                message:"Incorrect email or password.",
-                success:false,
-                        })
+                message: "Incorrect email or password.",
+                success: false,
+            })
         }
-        const isPasswordMatch=await bcrypt.compare(password,user.password);
-        if(!isPasswordMatch){
-           return res.status(400).json({
-                message:"Incorrect email or password.",
-                success:false,
-                        })  
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            return res.status(400).json({
+                message: "Incorrect email or password.",
+                success: false,
+            })
         };
         // check role is correct or not
-        if(role!=user.role){
+        if (role != user.role) {
             return res.status(400).json({
-                message:"Account does not exists with current role.",
-                success:false
+                message: "Account does not exists with current role.",
+                success: false
             })
         };
 
-        const tokenData={
-          userId:user._id  
+        const tokenData = {
+            userId: user._id
         }
-        const token=await jwt.sign(tokenData,process.env.SECRET_KEY,{expiresIn:'1d'});
+        const token = await jwt.sign(tokenData, process.env.SECRET_KEY, { expiresIn: '1d' });
 
-        user={
-           _id:user._id,
-           fullname:user.fullname,
-           email:user.email,
-           phoneNumber:user.phoneNumber,
-           role:user.role,
-           profile:user.profile
+        user = {
+            _id: user._id,
+            fullname: user.fullname,
+            email: user.email,
+            phoneNumber: user.phoneNumber,
+            role: user.role,
+            profile: user.profile
         }
 
-        return res.status(200).cookie("token",token,{maxAge:1*24*60*60*1000,httpsOnly:true,sameSite:'strict'}).json({
-            message:`Welcome back ${user.fullname}`,
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' }).json({
+            message: `Welcome back ${user.fullname}`,
             user,
-            success:true
+            success: true
         })
-    }catch(error){
-console.log(error);
+    } catch (error) {
+        console.log(error);
     }
 }
-export const logout =async (req,res)=>{
-    try{
-return res.status(200).cookie("token","",{maxAge:0}).json({
-    message:"Logged Out Successfully.",
-    success:true
-})
-    }catch(error){
-console.log(error);
+export const logout = async (req, res) => {
+    try {
+        return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+            message: "Logged Out Successfully.",
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
     }
 }
 
-export const updateProfile=async(req,res)=>{
-    try{
-const{fullname,email,phoneNumber,bio,skills}=req.body;
-const file=req.file;
-// if(!fullname || !email || !phoneNumber || !bio || !skills){
-//             return res.status(400).json({
-//                 message:"Something is missing",
-//                 success:false
-//             });
-//         };
+export const updateProfile = async (req, res) => {
+    try {
+        const { fullname, email, phoneNumber, bio, skills } = req.body;
+        const file = req.file;
 
-//cloudinary 
-let skillsArray;
-if(skills){
-skillsArray=skills.split(",");
-
-}
-
-const userId=req.id;
-let user=await User.findById(userId);
-
-if(!user){
-    return res.status(400).json({
-        message:"User not found.",
-        success:false
-    })
-}
-//updating data
-if(fullname) user.fullname=fullname
-if(email) user.email=email
-if(phoneNumber) user.phoneNumber=phoneNumber
-if(bio) user.profile.bio=bio
-if(skills) user.profile.skills=skillsArray
+        const fileUri = getDataUri(file);
+        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
 
 
-//resume comes later here
-await user.save();
-user={
-           _id:user._id,
-           fullname:user.fullname,
-           email:user.email,
-           phoneNumber:user.phoneNumber,
-           role:user.role,
-           profile:user.profile
+
+        let skillsArray;
+        if (skills) {
+            skillsArray = skills.split(",");
+
         }
-return res.status(200).json({
-    message:"Profile Updated Successfully.",
-    user,
-    success:true
-})
-    }catch(error){
-console.log(error);
+
+        const userId = req.id;
+        let user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(400).json({
+                message: "User not found.",
+                success: false
+            })
+        }
+        //updating data
+        if (fullname) user.fullname = fullname
+        if (email) user.email = email
+        if (phoneNumber) user.phoneNumber = phoneNumber
+        if (bio) user.profile.bio = bio
+        if (skills) user.profile.skills = skillsArray
+
+
+        //resume comes later here
+if(cloudResponse){
+    user.profile.resume = cloudResponse.secure_url
+    user.profile.resumeOriginalName=file.originalname
+}
+
+
+        await user.save();
+        user = {
+            _id: user._id,
+            fullname: user.fullname,
+            email: user.email,
+            phoneNumber: user.phoneNumber,
+            role: user.role,
+            profile: user.profile
+        }
+        return res.status(200).json({
+            message: "Profile Updated Successfully.",
+            user,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
     }
 }
--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -8,13 +8,15 @@ import { Label } from './ui/label'
 import AppliedJobTable from './AppliedJobTable'
 import { useState } from 'react'
 import UpdateProfileDialog from './UpdateProfileDialog'
+import { useSelector } from 'react-redux'
 
-const skills = ["HTML", "CSS", "Javascript", "React.js"];
+// const skills = ["HTML", "CSS", "Javascript", "React.js"];
+ const isResume = true;
 
 const Profile = () => {
-    const isResume = true;
     const [open, setOpen] = useState(false);
-    
+    const {user}=useSelector(store=>store.auth);
+
     return (
         <div>
             <Navbar />
@@ -26,8 +28,8 @@ const Profile = () => {
 
                         </Avatar>
                         <div>
-                            <h1 className='font-md text-xl'>Full Name</h1>
-                            <p>Lorem ipsum dolor sit amet.</p>
+                            <h1 className='font-md text-xl'>{user?.fullname}</h1>
+                            <p>{user?.profile.bio}</p>
                         </div>
                     </div>
                     <Button onClick={()=>setOpen(true)} className='text-right' variant="outline">
@@ -37,19 +39,19 @@ const Profile = () => {
                 <div className='my-5'>
                     <div className='flex items-center gap-3 my-2'>
                         <Mail />
-                        <span>sharma@gmail.com</span>
+                        <span>{user?.email}</span>
 
                     </div>
                     <div className='flex items-center gap-3 my-2'>
                         <Contact />
-                        <span>8888888888</span>
+                        <span>{user?.phoneNumber}</span>
                     </div>
                 </div>
                 <div className='my-5'>
                     <h1>Skills</h1>
                     <div className='flex items-center gap-1'>
                         {
-                            skills.length != 0 ? skills.map((item, index) => <Badge key={index}>{item}</Badge>) : <span>NA</span>
+                           user?.profile?.skills.length != 0 ? user?.profile?.skills.map((item, index) => <Badge key={index}>{item}</Badge>) : <span>NA</span>
                         }
                     </div>
 
@@ -57,7 +59,7 @@ const Profile = () => {
                 <div className='grid w-full max-w-sm items-center gap-1.5'>
                     <Label className='text-md font-bold'>Resume</Label>
                     {
-                        isResume ? <a target='blank' href='https://youtube.com' className='text-blue-500 w-full hover:underline cursor-pointer'> Rishabh Sharma </a> : <span>NA</span>
+                        isResume ? <a target='blank' href={user?.profile?.resume} className='text-blue-500 w-full hover:underline cursor-pointer'> {user?.profile?.resumeOriginalName}</a> : <span>NA</span>
                     }
                 </div>
 
